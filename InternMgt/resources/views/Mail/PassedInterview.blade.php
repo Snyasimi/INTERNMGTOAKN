aeb3a5e903a291f763f6c0ff1e7f38291b88024f
Snyasimi <nyasimisolo@gmail.com>
2023-03-15 09:59:18 +0300

    Added a layout for Emails

--- InternMgt/resources/views/Mail/PassedInterview.blade.php
+++ InternMgt/resources/views/Mail/PassedInterview.blade.php
@@ -1,16 +1,8 @@
+@extends('Mail.layout.layout')
+@section('content')
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
     <p> We are pleased to inform you that youre accepted for the internship opportunity</p>
     <p>click here to create a password for yoyr account :  {{$Signin_url}}</p>
     <p>click here to decline the offer : {{$Decline_url}}</p>
     
-</body>
-</html>
+@endsection('content')
---
+++ InternMgt/resources/views/Mail/layout/layout.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Layout</title>	
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="" rel="stylesheet" >
+	<script defer src="" ></script>
+	<meta name="description" content="Apply for Internship opportunities"
+	@vite('resources/css/app.css')	
+</head>
+
+
+<body>
+
+@yield('content')
+
+</body>
+<html>
