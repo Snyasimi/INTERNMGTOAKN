e62f7724dfd67caaa0929ec02b9686de7cf3a41b
Snyasimi <nyasimisolo@gmail.com>
2023-04-13 10:35:02 +0300

    removed frontend urls

--- InternMgt/routes/web.php
+++ InternMgt/routes/web.php
@@ -21,19 +21,6 @@ use App\Http\Controllers\{RolesController,
 */
 
 Route::get('/', function () {
-    return view('Mail.PassedInterview');
+    return redirect()->away('http://oaknet.internmanagementsystem.live:9005');
 });
-Route::get("Account/Signin",[AccountActivator::class,'active'])->name('Setpassword');
 
-Route::resource('/Apply',ApplicantsController::class);
-Route::resource('User',UsersController::class);
-Route::resource('Task',TaskController::class);
-Route::post('PasswordReset',[AccountActivator::class,'RequestPasswordReset'])->name('passwordreset');
-Route::get('Activate',[AccountActivator::class,'active'])->name('Activate');
-
-Route::resource('Role',RolesController::class);
-Route::resource('Position',PositionController::class);
-Route::get('Login',[LoginController::class,'Login'])->name("Login");
-Route::post('Login',[LoginController::class,'authenticate'])->name('AuthLogin');
-Route::get('Login/Sign',[LoginController::class,'register'])->name('SignUp');
-Route::get('supervisors',[UsersController::class,'myInterns'])->name('sup');
